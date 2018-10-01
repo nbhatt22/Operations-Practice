5ca74733601cebcfe16edf27f9d9000c582e388a
nbhatt22 <43207133+nbhatt22@users.noreply.github.com>
2018-10-01 08:32:36 -0400

    Add files via upload

---
+++ src/OperationsPractice.java
@@ -0,0 +1,29 @@
+
+public class OperationsPractice {
+
+	public static void main(String[] args) 
+	{
+		int number; //Data type then variable to declare variable
+		
+		number = 20;
+		System.out.println("initial number: " + number);
+		
+		number +=5; //number = number + 5
+		System.out.println("add 5: " + number);
+		
+		number /=2; //number = number / 2
+		System.out.println("divide by 2: " + number);
+		
+		number *=4; //number = number * 4
+		System.out.println("multiply by 4: " + number);
+		
+		number -=6; //number = number - 6
+		System.out.println("subtract 6: " + number);
+		
+		number %=2; //number = number / 2
+		System.out.println("modularly divide by 2: " + number);
+		
+		
+	}
+
+}
